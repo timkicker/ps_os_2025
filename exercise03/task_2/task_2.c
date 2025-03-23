1f24f3e3b8d6087f0739a9adaa5b68908719cf38
nopesos <maurits@van-staalduinen.com>
2025-03-23 20:33:08 +0100

    fix: improve memory handling and string manipulation in task_2.c

--- exercise03/task_2/task_2.c
+++ exercise03/task_2/task_2.c
@@ -8,12 +8,20 @@
 
 void string_shift_right(int arg_index, char* input, const char* original) {
 	size_t input_length = strlen(input);
-	char shifted_input[input_length];
+	//added +1 to include the null-terminator
+	//char shifted_input[input_length];
+	char shifted_input[input_length+1];
 
 	for (size_t char_index = 0; char_index < input_length; ++char_index) {
-		size_t new_position = (char_index + 2) / input_length;
+		//out of bounds safe guard by using modulo and having it wrap around again
+		//size_t new_position = (char_index + 2) / input_length;
+		size_t new_position = (char_index + 2) % input_length;
 		shifted_input[new_position] = input[char_index];
 	}
+
+	//ensure the null-terminator exists at the end of the shifted array
+	shifted_input[input_length] = '\0';
+
 
 	for (size_t char_index = 0; char_index < input_length; ++char_index) {
 		input[char_index] = toupper(input[char_index]);
@@ -27,15 +35,23 @@ int main(int argc, const char** argv) {
 	char* shared_strings[argc];
 	for (int arg_index = 0; arg_index < argc; ++arg_index) {
 		size_t arg_length = strlen(argv[arg_index]);
-		shared_strings[arg_index] = malloc(arg_length * sizeof(char));
+		
+		//again add 1 for null-terminator, otherwise strcpy would try and copy strings into buffers which are too small
+		//shared_strings[arg_index] = malloc(arg_length * sizeof(char));
+		shared_strings[arg_index] = malloc((arg_length+1) * sizeof(char));
 		if (shared_strings[arg_index] == NULL) {
 			fprintf(stderr, "Could not allocate memory.\n");
 			exit(EXIT_FAILURE);
 		}
+		//strcpy is trying to copy a string from argv[arg_index] to the destination, however this is too small, fixed by line 13
+		//strncpy(shared_strings[arg_index], argv[arg_index], arg_length + 1) would be safer as it knows how long to copy
 		strcpy(shared_strings[arg_index], argv[arg_index]);
+		
 	}
 
-	for (int arg_index = 0; arg_index <= argc; arg_index++) {
+	//use < because otherwise, argv[argc] = NULL resulting in an error
+	//for (int arg_index = 0; arg_index <= argc; arg_index++) {
+	for (int arg_index = 0; arg_index < argc; arg_index++) {
 		pid_t pid = fork();
 		if (pid == -1) {
 			perror("Could not create fork");
@@ -47,10 +63,19 @@ int main(int argc, const char** argv) {
 		}
 	}
 
+
+
+	//wrong usage of pthread_join, as pthread_join is for threads and not process
 	for (int arg_index = 0; arg_index <= argc; arg_index++) {
-		pthread_join(arg_index, NULL);
+		//pthread_join(arg_index, NULL);
+		waitpid(-1, NULL, 0); //wait for any child process
+
 	}
 
+	//no free command, meaning memory is not freed after usage and therefore lost
+	for (int arg_index = 0; arg_index < argc; arg_index++) {
+		free(shared_strings[arg_index]);
+	}
 	printf("Done.");
 	return EXIT_SUCCESS;
 }
