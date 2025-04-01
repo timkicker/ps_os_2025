f325faa45b4ea6530fa5fd37b16122d1ca2f23ad
nopesos <maurits@van-staalduinen.com>
2025-04-01 15:14:39 +0200

    Refactor client.c: improve error handling, update usage messages, and enhance code readability

--- exercise04/task_3/client.c
+++ exercise04/task_3/client.c
@@ -4,41 +4,48 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+#define MAX_PATH_LEN 1024
+#define MAX_MSG_LEN 1000
+
 int main(int argc, char **argv) {
-        if (argc != 2) {
-                fprintf(stderr, "Usage: ./client.c <client_name>\n");
-                return EXIT_FAILURE;
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <client_name>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    char fifo_path[MAX_PATH_LEN];
+    snprintf(fifo_path, sizeof(fifo_path), "/tmp/%s_csbb8841_fifo", argv[1]);
+
+    int fifo_fd = open(fifo_path, O_WRONLY);
+    if (fifo_fd == -1) {
+        perror("Error opening FIFO");
+        return EXIT_FAILURE;
+    }
+
+    char message[MAX_MSG_LEN];
+    while (1) {
+        printf("Message: ");
+        if (!fgets(message, sizeof(message), stdin)) {
+            fprintf(stderr, "Error reading message from stdin\n");
+            return EXIT_FAILURE;
         }
 
-        char fifo_path[1000];
-        snprintf(fifo_path, sizeof(fifo_path), "/tmp/%s_csbb8841_fifo", argv[1]);
-
-        int fifo_file_descriptor = open(fifo_path, O_WRONLY);
-        if (fifo_file_descriptor == -1) {
-                fprintf(stderr, "error opening fifo\n");
-                return EXIT_FAILURE;
+        size_t msg_len = strlen(message);
+        if (msg_len > 0 && message[msg_len - 1] == '\n') {
+            message[msg_len - 1] = '\n';
         }
 
-        char message[1000];
-        while(1) {
-                printf("Message: ");
-                if (fgets(message, sizeof(message), stdin) == NULL) {
-                        fprintf(stderr, "error reading message from stdin\n");
-                        return EXIT_FAILURE;
-                }
-
-                if (write(fifo_file_descriptor, message, strlen(message)) != (ssize_t)strlen(message)) {
-                        fprintf(stderr, "error sending message");
-                        return EXIT_FAILURE;
-                }
-
-                if (strlen(message) == 1) {
-                        printf("disconnect\n");
-                        break;
-                }
+        if (write(fifo_fd, message, msg_len) != (ssize_t)msg_len) {
+            perror("Error sending message");
+            return EXIT_FAILURE;
         }
 
-        close(fifo_file_descriptor);
+        if (msg_len == 1) {
+            printf("Disconnecting...\n");
+            break;
+        }
+    }
 
-        return EXIT_SUCCESS;
+    close(fifo_fd);
+    return EXIT_SUCCESS;
 }
